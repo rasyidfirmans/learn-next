804415faf361a1c4efe86032c357ffa281547fce
Rasyid Firmansyah <rasyidfirmansyah56@student.uns.ac.id>
2025-03-05 11:03:01 +0700

    fix: import path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import "../styles/globals.css"
-import AppShell from './components/layouts/AppShell';
+import AppShell from '../components/layouts/AppShell';
  
 export default function App({ Component, pageProps }: AppProps) {
   return (
